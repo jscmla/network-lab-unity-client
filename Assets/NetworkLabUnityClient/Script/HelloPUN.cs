05e6c8624adeef4736666a01e460e97b2c4968bf
jscmla <jscmla1118@gmail.com>
2020-06-24 20:22:27 +0900

    feat: enable to connect to photon unity network

---
+++ Assets/NetworkLabUnityClient/Script/HelloPUN.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class HelloPUN : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        // Connect by PhotonServerSettings.asset
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Try to connect to game server
+    public override void OnConnectedToMaster()
+    {
+        // Create or Join to room "HelloPUN"
+        PhotonNetwork.JoinOrCreateRoom("HelloPUN", new RoomOptions(), TypedLobby.Default);
+    }
+
+    // Instantiate Object
+    public override void OnJoinedRoom()
+    {
+        var v = new Vector2(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+        PhotonNetwork.Instantiate("PUNCube", v, Quaternion.identity);
+    }
+}
