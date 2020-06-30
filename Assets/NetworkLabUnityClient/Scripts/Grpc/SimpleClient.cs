d541ee6cdfa9b2a590e1481525c9e48700d75207
jscmla <jscmla1118@gmail.com>
2020-06-30 17:48:56 +0900

    feat: enable to connect to server from client

---
+++ Assets/NetworkLabUnityClient/Scripts/Grpc/SimpleClient.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Grpc.Core;
+using Simple.Grpc;
+
+public class SimpleClient : MonoBehaviour
+{
+    void Start()
+    {
+        Channel channel = new Channel("localhost:50051", ChannelCredentials.Insecure);
+        SimpleService.SimpleServiceClient client = new SimpleService.SimpleServiceClient(channel);
+
+        var response = client.SimpleSend(new SimpleRequest());
+        Debug.Log("text: " + response.Text);
+    }
+}
---
+++ Assets/NetworkLabUnityClient/Scripts/Grpc/SimpleServer.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using Grpc.Core;
+using Simple.Grpc;
+
+public class SimpleServer : MonoBehaviour
+{
+    class SimpleServiceImpl : SimpleService.SimpleServiceBase
+    {
+        public override Task<SimpleResponse> SimpleSend(SimpleRequest request, ServerCallContext context)
+        {
+            var text = "This is TEST!";
+
+            return Task.FromResult(new SimpleResponse { Text = text });
+        }
+    }
+
+    void Start()
+    {
+        Server server = new Server
+        {
+            Services = { SimpleService.BindService(new SimpleServiceImpl()) },
+            Ports = { new ServerPort("localhost", 50051, ServerCredentials.Insecure) }
+        };
+        server.Start();
+        Debug.Log("Start server.");
+    }
+}
